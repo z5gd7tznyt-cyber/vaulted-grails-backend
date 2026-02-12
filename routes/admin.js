6e1ed69817a20ed8efa4f900e5643a884625c0ea
z5gd7tznyt-cyber <z5gd7tznyt@privaterelay.appleid.com>
2026-02-11 21:51:55 -0500

    Delete routes directory

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder for admin routes
-router.get('/', (req, res) => {
-    res.json({ message: 'Admin routes - protected' });
-});
-
-module.exports = router;
--- routes/tickets.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder for ticket purchase routes
-router.get('/', (req, res) => {
-    res.json({ message: 'Ticket routes coming soon' });
-});
-
-module.exports = router;
